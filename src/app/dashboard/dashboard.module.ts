fdd211fa861a6284d7a9e09c2bb593823c935b2a
João Vítor C. da Rosa <inde03@rede.ulbra.br>
2022-06-18 20:29:27 -0300

    corret nome to name pt 2

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { MaterialModule } from '../material/material.module';
-import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
